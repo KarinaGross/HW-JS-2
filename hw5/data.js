0a65b3d92360ffa859463a3be83bccb2bbc9a015
Karina <karina.dragan4uk@yandex.ru>
2023-07-16 14:31:27 +0300

    home work 5

---
+++ hw5/data.js
@@ -0,0 +1,58 @@
+let data = `[
+    {
+        "web_pages": "http://www.ablaikhan.kz/", 
+        "domains": "ablaikhan.kz", 
+        "country": "Kazakhstan", 
+        "name": "Abylai Khan University", 
+        "state_province": null, 
+        "alpha_two_code": "KZ"
+    }, 
+    {
+        "web_pages": "http://www.agriun.almaty.kz/", 
+        "domains": "agriun.almaty.kz", 
+        "country": "Kazakhstan", 
+        "name": "Kazak State University of Agriculture", 
+        "state_province": null, 
+        "alpha_two_code": "KZ"
+    }, 
+    {
+        "web_pages": "http://www.aktsu.kz/", 
+        "domains": "aktsu.kz", 
+        "country": "Kazakhstan", 
+        "name": "Aktau State University", 
+        "state_province": null, 
+        "alpha_two_code": "KZ"
+    }, 
+    {
+        "web_pages": "http://www.atgu.kz/", 
+        "domains": "atgu.kz", 
+        "country": "Kazakhstan", 
+        "name": "Atyrau State University", 
+        "state_province": null, 
+        "alpha_two_code": "KZ"
+    }, 
+    {
+        "web_pages": "http://www.ektu.kz/", 
+        "domains": "ektu.kz", 
+        "country": "Kazakhstan", 
+        "name": "East Kazakstan State Technical University", 
+        "state_province": null, 
+        "alpha_two_code": "KZ"
+    }, 
+    {
+        "web_pages": "http://www.enu.kz/", 
+        "domains": "enu.kz", 
+        "country": "Kazakhstan", 
+        "name": "L.N. Gumilyov Eurasian National University", 
+        "state_province": null, 
+        "alpha_two_code": "KZ"
+    }, 
+    {
+        "web_pages": "http://www.eurazir.kz/", 
+        "domains": "eurazir.kz", 
+        "country": "Kazakhstan", 
+        "name": "Eurasian Institute of market", 
+        "state_province": null, 
+        "alpha_two_code": "KZ"
+    }
+]`
